d9b6f4bd09f028b6c20609b2109a451ac7190186
VlShost <113975751+VlShost@users.noreply.github.com>
2023-09-15 21:17:23 +0300

    add reading from com-port

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,12 @@
-import { useState } from 'react';
-
 import Serial from './espserial';
 
 import './App.css';
 
 function App() {
-  const [serialActive, setSerialActive] = useState(false);
-  const [baudRate, setBaudRate] = useState('115200');
-
-  const options = [
-    { value: '9600', label: '9600' },
-    { value: '14400', label: '14400' },
-    { value: '19200', label: '19200' },
-    { value: '28800', label: '28800' },
-    { value: '38400', label: '38400' },
-    { value: '57600', label: '57600' },
-    { value: '115200', label: '115200' },
-    { value: '230400', label: '230400' },
-    { value: '460800', label: '460800' },
-    { value: '921600', label: '921600' },
-  ];
-
-  // console.log(baudRate);
-
-  const handleConnect = () => {
-    if (!serialActive) {
-      setSerialActive(true);
-    } else {
-      setSerialActive(false);
-    }
-    // console.log(serialActive);
-  };
-
-  const handleChange = (e) => {
-    setBaudRate(e.target.value);
-  };
-
   return (
     <div className="App">
-      <main className="container">
-        <label>
-          Baud rate:
-          <select value={baudRate} onChange={handleChange}>
-            {options.map((option) => {
-              return (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        {!serialActive ? (
-          <button type="button" onClick={handleConnect}>
-            Disconnect
-          </button>
-        ) : (
-          <button type="button" onClick={handleConnect}>
-            Connect
-          </button>
-        )}
-      </main>
-      <Serial baudRate={baudRate} endLine={'\r\n'} charset={'utf8'} serialActive />
+      <main className="container"></main>
+      <Serial />
     </div>
   );
 }
--- src/components/espserial.js
+++ src/components/espserial.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-function EspSerial({ baudRate, endLine, charset, serialActive }) {
-  const [active, setActive] = useState(false);
-  // const [port, setPort] = useState(null);
+function EspSerial() {
+  const [serialActive, setSerialActive] = useState(false);
+  const [baudRate, setBaudRate] = useState('115200');
+  const [endLine, setEndLine] = useState('\r\n');
+  const [charset, setCharset] = useState('utf8');
+
+  const [port, setPort] = useState(null);
   const [writer, setWriter] = useState(null);
   const [reader, setReader] = useState(null);
 
@@ -10,16 +14,76 @@ function EspSerial({ baudRate, endLine, charset, serialActive }) {
   const textEncoder = new TextEncoder(charset);
   const closeReadLoop = useRef(null);
 
-  // console.log(baudRate);
-  // console.log(navigator.serial);
+  const options = [
+    { value: '9600', label: '9600' },
+    { value: '14400', label: '14400' },
+    { value: '19200', label: '19200' },
+    { value: '28800', label: '28800' },
+    { value: '38400', label: '38400' },
+    { value: '57600', label: '57600' },
+    { value: '115200', label: '115200' },
+    { value: '230400', label: '230400' },
+    { value: '460800', label: '460800' },
+    { value: '921600', label: '921600' },
+  ];
 
-  useEffect(() => {
-    const portOpen = async () => {
-      const port = await navigator.serial.requestPort();
-      await port.open({ baudRate });
-      console.log(port);
-    };
-  }, [baudRate]);
+  // console.log(baudRate);
+
+  const handleConnect = async () => {
+    if (!serialActive) {
+      setSerialActive(true);
+      try {
+        let availablePort = await navigator.serial.requestPort();
+        console.log(availablePort);
+        setPort(availablePort);
+        console.log(port);
+        await port.open({ baudRate, endLine, charset });
+
+        port.ondisconnect = (e) => {
+          port.close();
+        };
+
+        closeReadLoop(handleRead());
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleDisconnect = async () => {
+    if (serialActive) {
+      setSerialActive(false);
+      if (port.readable) reader.cancel();
+      try {
+        await closeReadLoop;
+        await port.close();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    setBaudRate(e.target.value);
+  };
+
+  const handleRead = async () => {
+    while (port.readable && serialActive) {
+      const reader = port.readable.getReader();
+      try {
+        while (true) {
+          const { value, done } = await reader.read();
+          if (done) break;
+          textDecoder.decode(value);
+        }
+      } catch (error) {
+        // readable stream error
+        console.log('onError', error);
+      } finally {
+        reader.releaseLock();
+      }
+    }
+  };
 
   const handleKeyPress = () => {};
 
@@ -27,7 +91,30 @@ function EspSerial({ baudRate, endLine, charset, serialActive }) {
 
   return (
     <div className="container">
-      <textarea rows="25" onKeyDown={handleKeyPress} onPaste={handlePaste}></textarea>
+      <label>
+        Baud rate:
+        <select value={baudRate} onChange={handleChange}>
+          {options.map((option) => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            );
+          })}
+        </select>
+      </label>
+      {serialActive ? (
+        <button type="button" onClick={handleDisconnect}>
+          Disconnect
+        </button>
+      ) : (
+        <button type="button" onClick={handleConnect}>
+          Connect
+        </button>
+      )}
+      <div>
+        <textarea rows="25" onKeyDown={handleKeyPress} onPaste={handlePaste}></textarea>
+      </div>
     </div>
   );
 }
